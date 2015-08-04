b77ffb43233d549024823ccecbb13696911516b7
Zach <zpearson@greenoughcom.com>
2015-08-04 13:53:08 -0400

    set up stylesheet, finsihed layout, added logo:

--- ui.R
+++ ui.R
@@ -2,16 +2,30 @@ library(shiny)
 library(shinydashboard)
 
 dashboardPage(skin="green",
+        
   dashboardHeader(title = "FWER Calculator"),
   
   dashboardSidebar(
     
-    sliderInput("confidenceLvl", "Choose Confidence Level:", 1, 100, 95)
+    img(src="logo.png",
+        width=230),
+    
+    h2("App settings:"),
+    
+    sliderInput("confidenceLvl", "Confidence Level:", 1, 100, 95),
+    
+    radioButtons(
+      "errorCorrection", "Error Correction Method:",
+      c("Bonferroni", "Benjamini Hochberg")
+    )
+    
   ),
   
   dashboardBody(
+    
+    includeCSS("www/custom.css"),
   
-    h2("Make smarter decisions. Calculate your A/B test p-values!"),
+    h2(strong("Step 1:"), "Calculate your A/B tests' p-values!"),
     
     fluidRow(
       box(
@@ -24,19 +38,35 @@ dashboardPage(skin="green",
         numericInput("sampleSize2", "Sample Size (Variation B)", 0),
         numericInput("noConv2", "Number of Conversions (Variation B)", 0)
         ),
+        br(),
+        submitButton(text = "Calculate p-value!", icon = NULL),
         width = 9
       ),
       box(
         title = "Your tests' p-values:",
         
-        width= 3
+        width= 3, height = 255
       )
     ),
     
+    h2(strong("Step 2:"), "Enter your tests' p-values in the box below."),
+    
     fluidRow(
+
       box(
-        title = "Or, if you already know all your p Values, enter them here:",
-        textInput("pVals", "P Value:")
+        textInput("pVals", "P Values:"),
+        width = 12
+      )
+      
+    ),
+    
+    h2(strong("Step 3:"), "Use your results to make smarter marketing decisions!"),
+    
+    fluidRow(
+      
+      box(
+        textInput("result", "The statistically significant test results are:"),
+        width = 12
       )
       
     )
